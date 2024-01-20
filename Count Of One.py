c5a853ca455b085597cffd2c7091d342901f81ad
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-01-20 20:28:07 +0530

    Update and rename Count Of One to Count Of One.py

--- Count Of One.py
+++ Count Of One.py
@@ -5,6 +5,4 @@ def getCountOfOneNumbers(L, R):
         if int(str(L)[0])==1:
             result+=1
         L+=1
-    return result
-
-#INPUT #OUTPUT print(getCountOfSantiagoNumbers(0, 500))
+    return result #INPUT #OUTPUT print(getCountOfSantiagoNumbers(0, 500))
