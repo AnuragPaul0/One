29114c7e9649d9d75d13f7af573031e02f30d4d6
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-02-07 20:04:05 +0530

    Create FACT.py

---
+++ FACT.py
@@ -0,0 +1,13 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+al = "abcdefghijklmnopqrstuvwxyz"
+s = input()
+ca = 1
+for c in al:
+    if c not in s.lower():
+        print("false") # F
+        ca = 0
+        break
+if ca:
+    print("true") # T
+print(s)
+# 0abcdefghijklmnopqrstuvwxyZ
