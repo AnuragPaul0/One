5e44d55fe6b578ffa3a05ebf2cccbf4066d245b7
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-09-29 18:39:42 +0530

    Update Count Of One.py 111

--- Py/Count Of One.py
+++ Py/Count Of One.py
@@ -1,8 +1,10 @@
 def getCountOfOneNumbers(L, R):
+    # SantiagoNumbers
     result = 0
     while(L<=R):
         if int(str(L)[0])==1:
             result+=1
         L+=1
     return result #INPUT #OUTPUT
-    # print(getCountOfSantiagoNumbers(0, 500))
+# print(getCountOfOneNumbers(0, 500))
+# 111
